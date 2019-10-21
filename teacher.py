175709b93094ed25de64290c7bb93d44e40e3eb2
Elijah Wiertel <wiertele@gilmour.org>
2019-10-21 13:26:03 -0400

    updated teacher.py

--- teacher.py
+++ teacher.py
@@ -20,6 +20,7 @@ class PiggyParent(gopigo3.GoPiGo3):
         self.scan_data = {}
         self.distance_sensor = EasyDistanceSensor()
         self.imu = inertial_measurement_unit.InertialMeasurementUnit(bus = "GPG3_AD1")
+        self.stop()
 
     def calibrate(self):
         """allows user to experiment on finding centered midpoint and even motor speeds"""
@@ -30,18 +31,18 @@ class PiggyParent(gopigo3.GoPiGo3):
             while True:
                 response = str.lower(input("Turn right, left, or am I done? (r/l/d): "))
                 if response == "r":
-                    self.MIDPOINT += 25
-                    print("Midpoint: " + str(self.MIDPOINT))
-                    self.servo(self.MIDPOINT)
-                elif response == "l":
                     self.MIDPOINT -= 25
                     print("Midpoint: " + str(self.MIDPOINT))
                     self.servo(self.MIDPOINT)
+                elif response == "l":
+                    self.MIDPOINT += 25
+                    print("Midpoint: " + str(self.MIDPOINT))
+                    self.servo(self.MIDPOINT)
                 else:
-                    print("Midpoint now saved to: " + str(self.MIDPOINT))
+                    print("Midpoint temporarily saved to: " + str(self.MIDPOINT) + "\nYou'll need to update your magic number.")
                     break
         else:
-            print('Okay, remember %d as the correct self.MIDPOINT' % self.MIDPOINT)
+            print('Cool, %d is the correct self.MIDPOINT' % self.MIDPOINT)
         response = str.lower(input("Do you want to check if I'm driving straight? (y/n)"))
         if 'y' in response:
             while True:
@@ -101,7 +102,7 @@ class PiggyParent(gopigo3.GoPiGo3):
         will rotate until the gyroscope reads 20."""
 
         # error check
-        goal = deg % 360
+        goal = abs(deg) % 360
         current = self.get_heading()
 
         turn = self.right  # connect it to the method without the () to activate
@@ -173,6 +174,5 @@ class PiggyParent(gopigo3.GoPiGo3):
     def get_heading(self):
         """Returns the heading from the IMU sensor"""
         reading = self.imu.read_euler()[0]
-        time.sleep(.05)
         print("Gyroscope sensor is at: {} degrees ".format(reading))
         return reading
