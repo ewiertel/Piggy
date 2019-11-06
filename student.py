4806ac706ed7ab53698c9e00a2fd16b6bb5df36c
Elijah Wiertel <wiertele@gilmour.org>
2019-11-06 10:35:38 -0500

    fixed navigation - set servo to midpoint each loop

--- student.py
+++ student.py
@@ -162,30 +162,30 @@ class Piggy(PiggyParent):
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         while True:
+            self.servo(self.MIDPOINT)
             while self.read_distance() > 250:
                 self.fwd()
                 time.sleep(0.1)
-            self.turn_by_deg(45)
-
-        self.scan()
-        # traversal
-        left_total = 0
-        left_count = 0
-        right_total = 0
-        right_count = 0
-        for ang, dist in enumerate(self.scan_data):
-            if ang < self.MIDPOINT:
-                right_total += dist
-                right_count += 1
+            self.stop()
+            self.scan()
+            # traversal
+            left_total = 0
+            left_count = 0
+            right_total = 0
+            right_count = 0
+            for ang, dist in enumerate(self.scan_data):
+                if ang < self.MIDPOINT:
+                    right_total += dist
+                    right_count += 1
+                else:
+                    left_total += dist
+                    left_count += 1
+            left_avg = left_total / left_count
+            right_avg = right_total / right_count
+            if left_avg > right_avg:
+                self.turn_by_deg(-45)
             else:
-                left_total += dist
-                left_count += 1
-        left_avg = left_total / left_count
-        right_avg = right_total / right_count
-        if left_avg > right_avg:
-            self.turn_by_deg(-45)
-        else:
-            self.turn_by_deg(45)
+                self.turn_by_deg(45)
 
 
 
