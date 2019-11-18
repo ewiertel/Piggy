e743f4c69ba186dbfd467cd1de2f4afa97984b2e
Elijah Wiertel <wiertele@gilmour.org>
2019-11-18 11:10:26 -0500

    fixed tabbing error

--- student.py
+++ student.py
@@ -167,43 +167,43 @@ class Piggy(PiggyParent):
         return True
 
     def nav(self):
-                "robot able to navigate by checking surroundings"
-        #assuming that we are facing the exit at the start
-        self.exit_heading = self.get_heading()      
+            "robot able to navigate by checking surroundings"
+            #assuming that we are facing the exit at the start
+            self.exit_heading = self.get_heading()      
 ​
-        print("-----------! NAVIGATION ACTIVATED !------------\n")
-        print("-------- [ Press CTRL + C to stop me ] --------\n")
-        print("-------------! EXIT IS AT %d !---------------\n" % self.exit_heading) 
-        corner_count = 0
-        while True:
-            self.servo(self.MIDPOINT) #return servo to the center 
-            while self.quick_check():
-                corner_count = 0
-                self.fwd()
-                time.sleep(.01)
-            self.stop()
-            self.scan() #go to scan method and check surroundings
-            #traversal
-            corner_count += 1
-            if corner_count > 3:
-                self.escape()
-            left_total = 0
-            left_count = 0
-            right_total = 0
-            right_count = 0
-            for ang, dist in self.scan_data.items():
-                if ang < self.MIDPOINT:
-                    right_total += dist
-                    right_count +=1
+            print("-----------! NAVIGATION ACTIVATED !------------\n")
+            print("-------- [ Press CTRL + C to stop me ] --------\n")
+            print("-------------! EXIT IS AT %d !---------------\n" % self.exit_heading) 
+            corner_count = 0
+            while True:
+                self.servo(self.MIDPOINT) #return servo to the center 
+                while self.quick_check():
+                    corner_count = 0
+                    self.fwd()
+                    time.sleep(.01)
+                self.stop()
+                self.scan() #go to scan method and check surroundings
+                #traversal
+                corner_count += 1
+                if corner_count > 3:
+                    self.escape()
+                left_total = 0
+                left_count = 0
+                right_total = 0
+                right_count = 0
+                for ang, dist in self.scan_data.items():
+                    if ang < self.MIDPOINT:
+                        right_total += dist
+                        right_count +=1
+                    else:
+                        left_total += dist
+                        left_count += 1
+                left_avg = left_total / left_count
+                right_avg = right_total / right_count
+                if left_avg > right_avg:
+                    self.turn_by_deg(-45)
                 else:
-                    left_total += dist
-                    left_count += 1
-            left_avg = left_total / left_count
-            right_avg = right_total / right_count
-            if left_avg > right_avg:
-                self.turn_by_deg(-45)
-            else:
-                self.turn_by_deg(45)
+                    self.turn_by_deg(45)
                 
     def escape(self):
         self.turn_by_deg(180)
