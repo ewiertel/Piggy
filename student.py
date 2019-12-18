8a4358b16f591df63a5d0c1eec3af3f8e32a8cac
Elijah Wiertel <wiertele@gilmour.org>
2019-12-18 09:05:22 -0500

    got rid f left and rightspeed vars

--- student.py
+++ student.py
@@ -205,14 +205,12 @@ class Piggy(PiggyParent):
         while self.get_heading() != starting_direction:
             # if need veer right
             if self.get_heading() < starting_direction:
-                right_speed -= 5
-                left_speed += 5
+                self.set_motor_power(self.MOTOR_LEFT, 90)
+                self.set_motor_power(self.MOTOR_RIGHT, 70)
             # if need veer left
             elif self.get_heading() > starting_direction:
-                left_speed -= 5
-                right_speed += 5
-            self.set_motor_power(self.MOTOR_LEFT, left_speed)
-            self.set_motor_power(self.MOTOR_RIGHT, right_speed)
+                self.set_motor_power(self.MOTOR_LEFT, 70)
+                self.set_motor_power(self.MOTOR_RIGHT, 90)
             time.sleep(.1)
         
 
